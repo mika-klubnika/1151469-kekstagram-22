101c68d0d49c09734f562aee9db4d50392be12ca
Марина <mika-klubnika@yandex.ru>
2021-02-02 23:29:08 +0300

    Доработаны функции

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-// с mdn
+'use strict';
+
 const getRandomNumber = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  if (min < 0 || max < 0 || min == max) {
+    return 'Введите положительные числа неравные друг другу';
+  } else if (min > max) {
+    return 'Введите числа от меньшего к большему';
+  } else {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+  }
+};
 
-getRandomNumber(2,6)
+getRandomNumber(3, 9);
+getRandomNumber(-5, -2);
+getRandomNumber(10, 2);
+getRandomNumber(1.5, 9.2);
 
+const checksLengthString = (comment, maxLengthComment) => {
+  maxLengthComment = 140;
 
-const checksLengthString = (comment) => {
-  if(comment.length < 140) {
+  if (comment.length < maxLengthComment) {
     return 'строка проходит по длине';
   } else {
     return 'строка не проходит по длине';
   }
-}
+};
 
 checksLengthString('Lorem ipsum dolor sit amet consectetur adipisicing');
-
