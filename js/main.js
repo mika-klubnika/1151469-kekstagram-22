73984bf31634fee0c9495a6eed43af4a2c6eec66
Марина <mika-klubnika@yandex.ru>
2021-02-03 13:20:24 +0300

    Правки в функциях

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
 'use strict';
 
 const getRandomNumber = (min, max) => {
-  if (min < 0 || max < 0 || min == max) {
-    return 'Введите положительные числа неравные друг другу';
-  } else if (min > max) {
-    return 'Введите числа от меньшего к большему';
-  } else {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
+  if (min < 0 || min >= max) {
+    return 0;
   }
+  return Math.floor((Math.random() * (max - min + 1)) + min);
 };
 
 getRandomNumber(3, 9);
-getRandomNumber(-5, -2);
-getRandomNumber(10, 2);
-getRandomNumber(1.5, 9.2);
 
 const checksLengthString = (comment, maxLengthComment) => {
-  maxLengthComment = 140;
-
-  if (comment.length < maxLengthComment) {
-    return 'строка проходит по длине';
-  } else {
-    return 'строка не проходит по длине';
+  if (comment.length > maxLengthComment) {
+    return false;
   }
+  return true;
 };
 
-checksLengthString('Lorem ipsum dolor sit amet consectetur adipisicing');
+checksLengthString('Lorem ipsum dolor sit amet consectetur adipisicing', 60);
